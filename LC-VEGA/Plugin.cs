796fab5d4e9f59f3f651a7e3b177e34594608052
03-JS <pyjoso3vitaslim@gmail.com>
2024-07-13 18:08:31 +0200

    This is v3 now

--- LC-VEGA/Plugin.cs
+++ LC-VEGA/Plugin.cs
@@ -29,7 +29,7 @@ namespace LC_VEGA
     {
         private const string modGUID = "JS03.LC-VEGA";
         private const string modName = "LC-VEGA";
-        private const string modVersion = "2.1.0";
+        private const string modVersion = "3.0.0";
 
         internal static AssetBundle assetBundle;
 
