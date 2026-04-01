f3bb32509a27b4296b6260eb7ede858448b46013
03-JS <pyjoso3vitaslim@gmail.com>
2026-04-01 13:54:36 +0200

    Fix apparatus pick up voice line

--- LC-VEGA/Patches/ApparatusPatch.cs
+++ LC-VEGA/Patches/ApparatusPatch.cs
@@ -7,11 +7,11 @@ namespace LC_VEGA.Patches
     internal class ApparatusPatch
     {
         [HarmonyPatch(typeof(LungProp), "EquipItem")]
-        [HarmonyPostfix]
-        static void PlayApparatusPulledLine()
+        [HarmonyPrefix]
+        static void PlayApparatusPulledLine(LungProp __instance)
         {
             // VEGA.facilityHasPower = false;
-            if (!Plugin.giveApparatusWarnings.Value) return;
+            if (!Plugin.giveApparatusWarnings.Value || !__instance.isLungDocked) return;
             if (Random.Range(0, 10) == 0) VEGA.PlayRandomLine("ApparatusPulledEasterEgg", Random.Range(1, 3), 3.65f);
             VEGA.PlayLine("ApparatusPulled", 3.65f);
         }
